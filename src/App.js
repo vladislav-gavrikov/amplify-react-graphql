323e2e59c6e0f8fe46bdf2f2fec892e6edce00cc
vladislav-gavrikov <vlad.gavr0303@gmail.com>
2023-04-06 14:38:05 +1000

    test

--- src/App.js
+++ src/App.js
@@ -17,6 +17,19 @@ function App() {
         >
           Learn React
         </a>
+<script type="text/javascript">
+  (function(w, d, x, id){
+    s=d.createElement('script');
+    s.src='https://d1nv5i00u1m742.cloudfront.net/amazon-connect-chat-interface-client.js';
+    s.async=1;
+    s.id=id;
+    d.getElementsByTagName('head')[0].appendChild(s);
+    w[x] =  w[x] || function() { (w[x].ac = w[x].ac || []).push(arguments) };
+  })(window, document, 'amazon_connect', '17361773-c37e-410c-a113-df2b89734015');
+  amazon_connect('styles', { openChat: { color: '#ffffff', backgroundColor: '#123456'}, closeChat: { color: '#ffffff', backgroundColor: '#123456'} });
+  amazon_connect('snippetId', 'QVFJREFIaDJSeExmZC9mdkhGcGpyYSs4Zm9SMXpVL2FlMFF2aEpVbzNKb3oyNCtZekFIV2h4dlVrcWRGbUQwVEYvNldZT3QwQUFBQWJqQnNCZ2txaGtpRzl3MEJCd2FnWHpCZEFnRUFNRmdHQ1NxR1NJYjNEUUVIQVRBZUJnbGdoa2dCWlFNRUFTNHdFUVFNbWFCdHI5Wi9laFVtSzZ2YUFnRVFnQ3ZhajlONjZuS3dGYnUzUkc2ejhHRGluZkxlWFF5VWRtU3JCdEpaLzIwbG9oS3NJdWIwYnFFY2Q1ckk6OnJBcmFRdXpCSGNYaktYWDlKZm5raDZLZHFsWWI2WTl0WmtXQ3hycVFvRHJ2OUM4bURxQVdhZ2ZvYzhuRmNnczlhTVBvMURTZEhKR09LQWZDbExUOUpteU1HNGc3S3hDQmFSWnBHMVFpamVwbUFtZ24vaGVPSmtiYmFOem9kZjJZeExFZ0N3SGNPRm4zeGJXcWlaK2p5L1BTSy83akZ0Zz0=');
+  amazon_connect('supportedMessagingContentTypes', [ 'text/plain', 'text/markdown' ]);
+</script>
       </header>
     </div>
   );
