6318255c8ceb6c0434b7bbbba75f4faf51fcf23a
vladislav-gavrikov <vlad.gavr0303@gmail.com>
2023-05-26 10:26:56 +1000

    test

--- src/App.js
+++ src/App.js
@@ -7,21 +7,11 @@ function App() {
     
     <div className="App">
       <header className="App-header">
-      
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          Sample CCP embeded app with Stream APIs
+        </h2>
         <div id="containerDiv" className="CCP-app"></div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          TEST TEST TEST
-        </a>
-        
+        <p>Firefox not working. Please use Chrome/Edge</p>   
       </header>
     </div>
   );
